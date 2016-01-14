a6e6729ba23c91a2251e95934dc23434f36a7547
vdumbrava <frenchfries17182@yahoo.com>
2016-01-14 15:09:10 +0200

    Test for CI on linux

--- src/features/step_definitions/my_steps.rb
+++ src/features/step_definitions/my_steps.rb
@@ -21,16 +21,16 @@ Before  do
   # )
 
   # Selenium::WebDriver::Chrome.driver_path = "e:/Training Automation/chromedriver.exe"
-  # @page = Abstract_page.new(Selenium::WebDriver.for :chrome)
+   @page = Abstract_page.new(Selenium::WebDriver.for :chrome)
 
-  @page = Abstract_page.new(Selenium::WebDriver.for :firefox)
+  # @page = Abstract_page.new(Selenium::WebDriver.for :firefox)
 
   #@page = Abstract_page.new(Selenium::WebDriver.for(:remote, :url => 'http://localhost:4444/wd/hub', :desired_capabilities => :firefox))
 
 end
 
 After do
-  @page.unMuteVolume
+  #@page.unMuteVolume
   @page.quit
 end
 
